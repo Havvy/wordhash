95ad2192d049de6fe62168972cf6068d36322580
Havvy <ryan.havvy@gmail.com>
2012-06-03 21:09:52 -0700

    Initial Commit
    We have a working CLI, and WordHash is clean.
    
    Frame should probably be mergd into MainGUI.

---
+++ MainGUI.java
@@ -0,0 +1,16 @@
+/**
+ * @author Havvy, Joesph Obanion
+ * @email ryan.havvy@gmail.com, moltow@gmail.com
+ */
+
+package assignment8;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MainGUI {
+	
+	public static void main (String[] argv) {
+		new Frame();
+	}
+}
